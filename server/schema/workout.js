1559346dc45c81ba6ba9b27d654d8b147945fd7d
diana <diana@jhubers-MacBook-Pro.local>
2026-05-26 11:10:46 -0700

    connected frontend to backend, get and post working

--- server/schema/workout.js
+++ server/schema/workout.js
@@ -8,4 +8,4 @@ const workoutSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 })
 
-module.exports = mongoose.model('Workout', workoutSchema)
+module.exports = mongoose.model('Workout', workoutSchema)o
